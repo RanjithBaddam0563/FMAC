9f0370f542268bdfa1f572d1b9029215a0be8c13
RanjithBaddam0563 <60254897+RanjithBaddam0563@users.noreply.github.com>
2020-12-30 17:34:54 +0530

    Initial Commit

---
+++ FMAC/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FMAC
+//
+//  Created by MicroExcel on 12/30/20.
+//  Copyright © 2020 Fujairah. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
